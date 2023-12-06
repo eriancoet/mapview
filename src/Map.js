5989f53432b1cbab2e7b2ef148be0016b411458f
Rian Coetzee <eriancoet@gmail.com>
2023-12-06 13:27:24 +0200

    Dec6exrc

--- src/Map.js
+++ src/Map.js
@@ -14,16 +14,17 @@ const AnyReactComponent = ({ imgSrc, text }) => (
 
 const Map = () => {
     const clients = [
-        { id: 1, lat: -33.9266, lng: 18.4499, img: markerImage1, name: "Jon Doe" },
-        { id: 2, lat: -33.9265, lng: 18.4495, img: markerImage2, name: "Jack Burn" }, 
-        { id: 3, lat: -33.9265, lng: 18.4487, img: markerImage2, name: "JJoe Cocker" },
-        { id: 4, lat: -33.9265, lng: 18.4480, img: markerImage2, name: "BoesMan Mob" },     
-        { id: 5, lat: -33.9264, lng: 18.4475, img: markerImage2, name: "Koos Kombuis" },
-        { id: 6, lat: -33.9266, lng: 18.4470, img: markerImage2, name: "Manowar Chal" },
-        { id: 7, lat: -33.9266, lng: 18.4460, img: markerImage1, name: "Goldie Spree" },
-        { id: 8, lat: -33.9266, lng: 18.4455, img: markerImage2, name: "Kally Softmore" },
-        { id: 9, lat: -33.9266, lng: 18.4450, img: markerImage1, name: "Floorwis Butch" },
-        { id: 10, lat: -33.9266, lng: 18.4444, img: markerImage1, name: "Buck Marlsee" },
+        { id: 0, lat: -33.9266, lng: 18.4528, img: markerImage1, name: "Jonnie Darko" },
+        { id: 1, lat: -33.9266, lng: 18.4523, img: markerImage2, name: "Jon Doe" },
+        { id: 2, lat: -33.9265, lng: 18.4518, img: markerImage1, name: "Jack Burn" }, 
+        { id: 3, lat: -33.9265, lng: 18.4510, img: markerImage1, name: "JJoe Cocker" },
+        { id: 4, lat: -33.9265, lng: 18.4499, img: markerImage2, name: "BoesMan Mob" },     
+        { id: 5, lat: -33.9264, lng: 18.4490, img: markerImage2, name: "Koos Kombuis" },
+        { id: 6, lat: -33.9264, lng: 18.4480, img: markerImage2, name: "Manowar Chal" },
+        { id: 7, lat: -33.9264, lng: 18.4470, img: markerImage1, name: "Goldie Spree" },
+        { id: 8, lat: -33.9264, lng: 18.4465, img: markerImage2, name: "Kally Softmore" },
+        { id: 9, lat: -33.9264, lng: 18.4450, img: markerImage1, name: "Floorwis Butch" },
+        { id: 10, lat: -33.9264, lng: 18.4444, img: markerImage1, name: "Buck Marlsee" },
       // ... add more clients
     ];
   
