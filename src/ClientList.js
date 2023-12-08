dbe8c10d16a41f75efa05a96b610bbaef928f586
Rian Coetzee <eriancoet@gmail.com>
2023-12-08 18:48:38 +0200

    Marker direction 1

--- src/ClientList.js
+++ src/ClientList.js
@@ -1,17 +1,18 @@
 import React from "react";
 import './ClientList.css'; // Import the CSS for styling
+import icon from './icon.png';
 
 const data = [
-  { name: 'John Doe', address: 'Albert road, woodstock', hits: 3, lastSeen: '2023-01-01' },
-  { name: 'Jane Smith', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'JJ Odie', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Jan Boom', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Koos Kombuis', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Manowar Chal', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Goldie Spree', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Kally Softmore', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Floors Fisher', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
-  { name: 'Buck Marlsee', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02' },
+  { name: 'John Doe', address: 'Albert road, woodstock', lastSeen: '2023-01-01', directions: './icon.png' },
+  { name: 'Jane Smith', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'JJ Odie', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'Jan Boom', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'Koos Kombuis', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'Manowar Chal', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'Goldie Spree', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png'},
+  { name: 'Kally Softmore', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'Floors Fisher', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
+  { name: 'Buck Marlsee', address: "Albert road, woodstock", hits: 2, lastSeen: '2023-01-02', directions: './icon.png' },
 
   // ... other data entries
 ];
@@ -21,19 +22,22 @@ function ClientList() {
     <div className="grid-container">
       <div className="grid-heading">Name</div>
       <div className="grid-heading">Address</div>
-      <div className="grid-heading">Hits</div>
+      <div className="grid-heading">Directions</div>
       <div className="grid-heading">Last Seen</div>
 
       {data.map((item, index) => (
-        <>
-          <div key={`name-${index}`} className="grid-item">{item.name}</div>
-          <div key={`attempts-${index}`} className="grid-item">{item.address}</div>
-          <div key={`hits-${index}`} className="grid-item">{item.hits}</div>
-          <div key={`lastseen-${index}`} className="grid-item">{item.lastSeen}</div>
-        </>
+        <React.Fragment key={index}>
+          <div className="grid-item">{item.name}</div>
+          <div className="grid-item">{item.address}</div>
+          <div className="grid-item">
+            <img src={icon} alt="Directions" />
+          </div>
+          <div className="grid-item">{item.lastSeen}</div>
+        </React.Fragment>
       ))}
     </div>
   );
 }
+
 
 export default ClientList;
